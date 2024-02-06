7eef135abe7c53c49bbd2f64b9609b8183186940
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-02-06 14:03:24 +0300

    Add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,31 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+item1 = Item("Смартфон", 10000, 20)
+item2 = Item("Ноутбук", 20000, 5)
+
+
+def test_init():
+    assert item1.name == "Смартфон"
+    assert item2.price == 20000
+    assert item2.quantity == 5
+
+
+def test_init_calculate_total_price():
+    assert item1.calculate_total_price() == 200000
+
+
+def test_init_apply_discount():
+    Item.pay_rate = 0.8
+    item1.apply_discount()
+    assert item2.price == 20000
+    assert item1.price == 8000
+
+
+
+
+
+
+
+
+
