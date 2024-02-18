7c31973ddf49e86a2ff8e5b23fa440be38a900e3
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-02-18 15:17:18 +0300

    add add method

--- src/item.py
+++ src/item.py
@@ -38,6 +38,13 @@ class Item:
     def __str__(self):
         return f"{self.__name}"
 
+    def __add__(self, other):
+        if isinstance(other, Item):
+            return self.quantity + other.quantity
+        else:
+            raise ValueError('Складывать можно только объекты Item и дочерние от них.')
+
+
     @property
     def name(self):
         '''
