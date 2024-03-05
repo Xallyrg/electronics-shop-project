0f5f35334064da2ec6a0a6f389a5987112ee3533
Oleg Zaslavskii <Zasoleg1995@gmail.com>
2024-03-05 12:47:11 +0300

    add tests to keyboard class

---
+++ tests/test_keyboard.py
@@ -0,0 +1,26 @@
+"""Здесь надо написать тесты с использованием pytest для модуля keyboard."""
+from src.item import Item
+from src.keyboard import Keyboard
+import pytest
+
+keyboard1 = Keyboard('клава', 15000, 10)
+
+
+def test_init():
+    assert keyboard1.name == "клава"
+    assert keyboard1.price == 15000
+    assert keyboard1.quantity == 10
+    assert keyboard1.language == 'en'
+
+
+def test_try_to_change_lang():
+    with pytest.raises(AttributeError):
+        keyboard1.language = 'eee'
+
+
+def test_change_lang_by_method():
+    assert keyboard1.language == 'en'
+    keyboard1.change_lang()
+    assert keyboard1.language == 'ru'
+    keyboard1.change_lang()
+    assert keyboard1.language == 'en'
